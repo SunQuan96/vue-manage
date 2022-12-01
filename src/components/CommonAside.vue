7c700931c8505eeae8622a215a87cc2501680a11
SunQuan96 <1097610301sq@gmail.com>
2022-12-01 18:53:44 +0800

    完结

--- src/components/CommonAside.vue
+++ src/components/CommonAside.vue
@@ -45,53 +45,10 @@
 </style>
 
 <script>
+import Cookie from "js-cookie"
 export default {
   data() {
     return {
-
-      menu: [
-        {
-          path: '/',
-          name: 'home',
-          label: '首页',
-          icon: 's-home',
-          url: 'Home/Home',
-        },
-        {
-          path: '/mall',
-          name: 'mall',
-          label: '商品管理',
-          icon: 'video-play',
-          url: 'MallManage/MallManage',
-        },
-        {
-          path: '/user',
-          name: 'user',
-          label: '用户管理',
-          icon: 'user',
-          url: 'UserManage/UserManage',
-        },
-        {
-          label: '其他',
-          icon: 'location',
-          children: [
-            {
-              path: '/page1',
-              name: 'page1',
-              label: '页面1',
-              icon: 'setting',
-              url: 'Other/PageOne',
-            },
-            {
-              path: '/page2',
-              name: 'page2',
-              label: '页面2',
-              icon: 'setting',
-              url: 'Other/PageTwo',
-            },
-          ]
-        }
-      ]
     };
   },
   methods: {
@@ -115,11 +72,15 @@ export default {
   computed: {
     //没有子菜单
     noChildren() {
-      return this.menu.filter(item => !item.children)
+      return this.menuData.filter(item => !item.children)
     },
     //有子菜单
     hasChildren() {
-      return this.menu.filter(item => item.children)
+      return this.menuData.filter(item => item.children)
+    },
+    menuData() {
+      // 判断当前数据,如果缓存中没有,当前store中去获取
+      return JSON.parse(Cookie.get('menu')) || this.$store.state.tab.menu
     },
     isCollapse() {
       return this.$store.state.tab.isCollapse
--- src/router/index.js
+++ src/router/index.js
@@ -13,13 +13,14 @@ const routes = [
   {
     path: "/",
     component: Main,
+    name: "Main",
     redirect: "/home", //重定向
     children: [
-      { path: "user", name: "user", component: User }, //用户管理
-      { path: "home", name: "home", component: Home }, //首页
-      { path: "mall", name: "mall", component: Mall }, //商品管理
-      { path: "page1", name: "page1", component: PageOne },
-      { path: "page2", name: "page2", component: PageTwo },
+      // { path: "user", name: "user", component: User }, //用户管理
+      // { path: "home", name: "home", component: Home }, //首页
+      // { path: "mall", name: "mall", component: Mall }, //商品管理
+      // { path: "page1", name: "page1", component: PageOne },
+      // { path: "page2", name: "page2", component: PageTwo },
     ],
   },
   {
